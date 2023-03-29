b5dc0a72061283b0ca0b7136c4828f90a9f02f63
Max PhoeniX <max.phoenix@hotmail.com>
2023-03-28 23:18:51 -0300

    Direto da pasta - obrigado Marcelo

--- Gateway_Heindall/Data/PrincipalContext.cs
+++ Gateway_Heindall/Data/PrincipalContext.cs
@@ -10,11 +10,10 @@ namespace Gateway_Heindall.Data
        : base(options) => Database.EnsureCreated();
 
         public DbSet<Grupo> Grupos { get; set; }
-        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Integrador> Integradores { get; set; }
         public DbSet<IntegradordoUser> IntegradoresdoUser { get; set; }
         public DbSet<IntegradordoUserTemp> IntegradoresdoUserTemp { get; set; }
 
-
+        //public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
